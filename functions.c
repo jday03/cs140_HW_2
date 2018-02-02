d3d9bd75c87359fcf9d94e4ace570eb688afed7d
jday03 <E!n$t31n>
2018-02-02 02:03:35 -0800

    updated powermethod function

--- functions.c
+++ functions.c
@@ -30,5 +30,72 @@ void generatevec(double * x,int size)
 // Subroutine for the power method, to return the spectral radius
 double powerMethod(double * mat, double * x, int size, int iter)
 {
+  int n = sqrt (size);
+  MPI_Bcast(&x, size, MPI_INT, 0, MPI_COMM_WORLD);
+double * newVec;
+// CALL newVec = multiply_Matrix();
+
+// USE GATHER TO GET ALL THE NEW VECTOR VALUES
+
+
+  MPI_Gather(newVec, 1, MPI_INT, x, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  // gotta figure out the arguments
+
+  // NEW UPDATE OF VECTOR SO WE CAN SQUARE THE SHIT.
+  MPI_Bcast(&x, size, MPI_INT, 0, MPI_COMM_WORLD);
+
+double* square;
+  square = squareVector();
+
+
+// USE REDUCE TO GET THE SQUARES BACK IN SUM FORM
+//if proc is 1
+  // go ahead and get that new vec/ magnitude
+
+// Next is to get the
   return lambda;
 }
+
+double* multiply_Matrix(double * mat, double * x, int n){
+
+
+  double *returnMatrix;
+  returnMatrix = new [n/ PROC] double ;
+    for(int rowCount = 0; rowCount < n; ++rowCount){
+      *returnMatrix[rowCount] = 0;
+      for(int colCount = 0; colCount < n; ++colCount) {
+        *returnMatrix[rowCount] += *(mat+colCount) * (x+colCount);
+      }
+
+    }
+
+  return returnMatrix;
+}
+
+
+
+
+
+
+double squareVector(double * x, int n){
+  double returnValues;
+  returnValues = 0;
+
+  for(int count = 0;count < n; count++){
+    returnValues[count] += *(x+count) *(x+count);
+  }
+
+  return returnValues;
+}
+
+
+
+
+
+void broadcastVector(double * vector,int size){
+
+
+
+
+}
+
